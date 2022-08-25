bf403bfe4c90d080000c5d4ad8959b03ce86841c
DaviMPaiva <davimpaiva@outlook.com>
2022-08-25 19:55:17 -0300

    pessoa_tb

---
+++ pessoa_tb.sql
@@ -0,0 +1,32 @@
+create table Pessoa_tb (cpf varchar2(11), nome varchar(100), idade integer, 
+                        CONSTRAINT Pessoa_tb_pk PRIMARY KEY (cpf)); 
+                        
+insert into Pessoa_tb (cpf,nome,idade)
+    values ('71311506578','Heitor Yuri da Silva',28);
+    
+insert into Pessoa_tb (cpf,nome,idade)
+    values ('44527288989','Geraldo Elias Rezende',57);
+    
+insert into Pessoa_tb (cpf,nome,idade)
+    values ('32008422704','Sônia Márcia Valentina Galvão',43);
+
+insert into Pessoa_tb (cpf,nome,idade)
+    values ('54657828622','Bernardo Thomas da Cruz',19);
+
+insert into Pessoa_tb (cpf,nome,idade)
+    values ('03757527062','Andreia Emily Lara Monteiro',32);
+    
+insert into Pessoa_tb (cpf,nome,idade)
+    values ('64145381084','Daniel Samuel Nelson Souza',25);
+    
+insert into Pessoa_tb (cpf,nome,idade)
+    values ('01280004223','Cláudio Carlos Eduardo Nogueira',37);
+
+insert into Pessoa_tb (cpf,nome,idade)
+    values ('73641828619','Isaac Heitor Lucca Martins',42);
+    
+insert into Pessoa_tb (cpf,nome,idade)
+    values ('14121741633','Giovanna Catarina Laura da Mota',39);
+    
+insert into Pessoa_tb (cpf,nome,idade)
+    values ('34988775267','Isabella Vanessa Ferreira',53);
