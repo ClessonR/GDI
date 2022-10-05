e936df092d27b770a4cc78f6143387275062df92
Jose Lucas Acioly <lucasacioly@outlook.com>
2022-10-05 17:30:30 -0300

    populate works

--- OR/create_tables.sql
+++ OR/create_tables.sql
@@ -1,10 +1,3 @@
-/*CREATE TABLE tb_endereco OF tp_endereco(
-    cep PRIMARY KEY,
-    rua NOT NULL,
-    numero NOT NULL,
-    cidade NOT NULL
-);*/
-
 CREATE TABLE tb_passageiro of tp_passageiro(
     cpf PRIMARY KEY, 
     nome NOT NULL,
@@ -12,16 +5,16 @@ CREATE TABLE tb_passageiro of tp_passageiro(
     telefones NOT NULL,
     endereco NOT NULL,
     fidelidade NOT NULL CHECK(fidelidade = 1 OR fidelidade = 0)
-) NESTED TABLE lista_bagagens STORE AS bagagem_passageiro, NESTED TABLE lista_compras STORE AS compra_passagens (NESTED TABLE lista_passagens STORE AS passagens_pasageiro);
- 
+) NESTED TABLE lista_bagagens STORE AS bagagem_passageiro;
 /
+
 CREATE TABLE tb_tripulante OF tp_tripulante(
     cpf PRIMARY KEY, 
     nome NOT NULL,
     idade NOT NULL,
     telefones NOT NULL,
     endereco NOT NULL,
-    cargo NOT NULL,
+    cargo NOT NULL CHECK (cargo = 'Piloto' or cargo = 'Comissário'),
     cadastro NOT NULL,
     salario NOT NULL,
     supervisor SCOPE IS tb_tripulante
@@ -47,26 +40,29 @@ CREATE TABLE tb_voo OF tp_voo (
     portao NOT NULL,
     local_partida NOT NULL, 
     local_chegada NOT NULL
-) NESTED TABLE lista_passagens STORE AS passagens_voo;
-/
-
-/*CREATE TABLE tb_passagem OF tp_passagem (
-    id PRIMARY KEY,
-    assento NOT NULL  
 );
 /
 
 CREATE TABLE tb_compra OF tp_compra(
     id_compra PRIMARY KEY,
     valor NOT NULL,
-    desconto WITH ROWID REFERENCES tb_cia_aerea NOT NULL
+    desconto WITH ROWID REFERENCES tb_cia_aerea NOT NULL,
+    passageiro WITH ROWID REFERENCES tb_passageiro NOT NULL
 );
-/*/
+/
+
+CREATE TABLE tb_passagem OF tp_passagem (
+    passagem_id PRIMARY KEY,
+    assento NOT NULL,
+    voo WITH ROWID REFERENCES tb_voo NOT NULL,
+    compra WITH ROWID REFERENCES tb_compra NOT NULL
+);
+/ 
 
 CREATE TABLE tb_trabalha OF tp_trabalha (
     tripulante WITH ROWID REFERENCES tb_tripulante NOT NULL,
     cia_aerea WITH ROWID REFERENCES tb_cia_aerea NOT NULL,
-    data_trabalha PRIMARY KEY
+    data_trabalha NOT NULL
 
 );
 /
